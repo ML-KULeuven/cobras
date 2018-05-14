8b86c6500ebc9802ece32cd2aaf13d6daecd94b4
toon <toonvancraenendonck@gmail.com>
2018-05-14 13:36:42 +0200

    adding the regular (i.e. non time-series) cobras

---
+++ cobras_ts/cobras_kmeans.py
@@ -0,0 +1,36 @@
+import numpy as np
+from cobras_ts.superinstance_kmeans import SuperInstance_kmeans
+from cobras_ts.cobras import COBRAS
+
+from sklearn.cluster import KMeans
+
+class COBRAS_kmeans(COBRAS):
+
+    def split_superinstance(self, si, k):
+        data_to_cluster = self.data[si.indices, :]
+        km = KMeans(k)
+        km.fit(data_to_cluster)
+
+        split_labels = km.labels_.astype(np.int)
+
+        training = []
+        no_training = []
+
+        for new_si_idx in range(len(set(split_labels))):
+            # go from super instance indices to global ones
+            cur_indices = [si.indices[idx] for idx, c in enumerate(split_labels) if c == new_si_idx]
+
+            si_train_indices = [x for x in cur_indices if x in self.train_indices]
+            if len(si_train_indices) != 0:
+                training.append(SuperInstance_kmeans(self.data, cur_indices, self.train_indices))
+            else:
+                no_training.append((cur_indices, np.mean(self.data[cur_indices,:],axis=0)))
+
+        for indices, centroid in no_training:
+            closest_train = max(training, key=lambda x: self.data[x.representative_idx, centroid])
+            closest_train.indices.extend(indices)
+
+        return training
+
+    def create_superinstance(self, indices):
+        return SuperInstance_kmeans(self.data, indices, self.train_indices)
